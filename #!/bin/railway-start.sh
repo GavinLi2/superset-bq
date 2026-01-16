a85faeadf4425a50d3583d20e8ac94c8ae834124
GavinLi2 <53229009+GavinLi2@users.noreply.github.com>
2026-01-16 10:57:07 +0800

    Add railway-start.sh for Superset setup

---
+++ #!/bin/railway-start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+superset db upgrade
+superset fab create-admin \
+  --username admin \
+  --firstname Admin \
+  --lastname User \
+  --email "$ADMIN_EMAIL" \
+  --password "$ADMIN_PASSWORD" || true
+superset init
+
+gunicorn --bind 0.0.0.0:8088 "superset.app:create_app()"
